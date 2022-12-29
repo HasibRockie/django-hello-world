9b699f821c5fbbd116c2fd3a00580a750ed5003c
Hasib Rockie <hasiee8004@gmail.com>
2022-12-29 08:27:37 +0600

    updated

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-wsi-8s_(i%zzq$l*mo&l3txmf0_7(qjzwi!nq5c^d+^6fe(dsf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
 
 # Application definition
@@ -127,3 +128,6 @@ CORS_ALLOWED_ORIGINS = ["https://example.com",
     "http://127.0.0.1:9000",
     "https://hasibmania.web.app",
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
